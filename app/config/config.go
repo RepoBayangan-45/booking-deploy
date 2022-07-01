d9472cb6fd591fd8d4522d27a9f728533eee45f1
Ikhsan Endang Prasetya <62880102+San-Kun@users.noreply.github.com>
2022-07-01 16:39:34 +0700

    Update config.go
    update config

--- app/config/config.go
+++ app/config/config.go
@@ -27,7 +27,7 @@ func InitDB() *gorm.DB {
 		DB_Name:     "45_office_booking",
 	}
 
-	connectionString := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?ssl-mode=REQUIRED",
+	connectionString := fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?charset=utf8mb4&parseTime=True&loc=Local",
 
 		config.DB_Username,
 		config.DB_Password,
