6487acf1f3840e39b34fcf6ade9adec9bc2383e9
Ikhsan Endang Prasetya <62880102+San-Kun@users.noreply.github.com>
2022-07-04 22:49:02 +0700

    Update user_create_response.go

--- domain/users/response/user_create_response.go
+++ domain/users/response/user_create_response.go
@@ -1,11 +1,8 @@
 package response
 
-import "time"
-
 type UserCreateResponse struct {
 	ID           int    `json:"id"`
 	Email        string `json:"email"`
 	Name         string `json:"name"`
 	Phone        string `json:"phone"`
-	TanggalLahir time.Time
 }
