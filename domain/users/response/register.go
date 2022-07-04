0946920f5db93e0d0958b718deb8c5b2af2d7d3f
Ikhsan Endang Prasetya <62880102+San-Kun@users.noreply.github.com>
2022-07-04 22:42:46 +0700

    Update register.go

--- domain/users/response/register.go
+++ domain/users/response/register.go
@@ -9,6 +9,5 @@ type SuccessRegister struct {
 	Email        string `json:"email" form:"email"`
 	Name         string `json:"name" form:"name"`
 	Phone        string `json:"phone" form:"phone"`
-	TanggalLahir time.Time
 	Password     string `json:"password" form:"password"`
 }
