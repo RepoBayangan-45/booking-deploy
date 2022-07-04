4a66fcb60218b2b5a8a3b3f956772106ca79aead
Ikhsan Endang Prasetya <62880102+San-Kun@users.noreply.github.com>
2022-07-04 22:42:03 +0700

    Update register.go

--- domain/users/request/register.go
+++ domain/users/request/register.go
@@ -5,7 +5,6 @@ import "time"
 type RegisterRequest struct {
 	Email        string `json:"email"`
 	Name         string `json:"name"`
-	TanggalLahir time.Time
 	Phone        string `json:"phone"`
 	Password     string `json:"password"`
 }
