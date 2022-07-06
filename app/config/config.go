28192d2c7d7b0f9f07d6cdebbba6ea277ef9fd79
Ikhsan Endang <ikhsanendang94@gmail.com>
2022-07-06 14:13:16 +0700

    update

--- app/config/config.go
+++ app/config/config.go
@@ -1,7 +1,11 @@
 package config
 
 import (
-	domain "Office-Booking/domain/users"
+	_ge "Office-Booking/domain/gedung"
+	_je "Office-Booking/domain/jenisgedung"
+	_ne "Office-Booking/domain/nearby"
+	_re "Office-Booking/domain/review"
+	_us "Office-Booking/domain/users"
 	"fmt"
 
 	"gorm.io/driver/mysql"
@@ -55,5 +59,9 @@ func InitDB() *gorm.DB {
 }
 
 func InitialMigration() {
-	DB.AutoMigrate(&domain.User{})
+	DB.AutoMigrate(&_us.User{})
+	DB.AutoMigrate(&_ge.Gedung{})
+	DB.AutoMigrate(&_je.Jenisgedung{})
+	DB.AutoMigrate(&_ne.Nearby{})
+	DB.AutoMigrate(&_re.Review{})
 }
