987b221404d40292f40f57cd4d2424796ca6f735
Ikhsan Endang Prasetya <62880102+San-Kun@users.noreply.github.com>
2022-07-06 14:41:00 +0700

    Update config.go

--- app/config/config.go
+++ app/config/config.go
@@ -32,7 +32,6 @@ func InitDB() *gorm.DB {
 	}
 
 	connectionString := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local",
-
 		config.DB_Username,
 		config.DB_Password,
 		config.DB_Port,
