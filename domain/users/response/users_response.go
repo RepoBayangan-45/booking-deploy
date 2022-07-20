af6b00d88fdf07ba565274366c23019b3b13d219
Ikhsan Endang <ikhsanendang94@gmail.com>
2022-07-20 13:45:12 +0700

    update

--- domain/users/response/users_response.go
+++ domain/users/response/users_response.go
@@ -1,13 +1,10 @@
 package response
 
-import "time"
-
 type UsersResponse struct {
-	ID        int    `json:"id"`
-	Email     string `json:"email"`
-	Name      string `json:"name"`
-	Fullname  string `json:"fullname"`
-	Alamat    string `json:"alamat"`
-	Phone     string `json:"phone"`
-	CreatedAt time.Time
+	ID       int    `json:"id"`
+	Email    string `json:"email"`
+	Name     string `json:"name"`
+	Fullname string `json:"fullname"`
+	Alamat   string `json:"alamat"`
+	Phone    string `json:"phone"`
 }
